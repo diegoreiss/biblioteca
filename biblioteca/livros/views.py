7714554d21ee0b7cd85021d130be06cf9b0c12a7
diegoreiss <diegoportal.reis@outlook.com>
2023-11-27 23:05:04 -0300

    add proteção contra adicionar livro sem autor ou genero

--- biblioteca/livros/views.py
+++ biblioteca/livros/views.py
@@ -1,10 +1,17 @@
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
 
+from .models import Autor, Genero
+
 
 @login_required
 def pagina_inicial(request):
-    return render(request, 'livros/pagina_inicial.html')
+    context = {
+        "autores": Autor.objects.all(),
+        "generos": Autor.objects.all()
+    }
+
+    return render(request, 'livros/pagina_inicial.html', context=context)
 
 
 @login_required
--- biblioteca/static/livros/pagina_inicial.js
+++ biblioteca/static/livros/pagina_inicial.js
@@ -21,3 +21,37 @@ buttonAdicionarLivro.on("click", (e) => {
   }
   limparFormAdicionarLivro();
 });
+
+const showToast = (color, title, body) => {
+  const myToast = $("#myToast");
+
+  if (myToast) {
+    myToast.remove();
+  }
+
+  $("body").append(`
+  <div id="myToast" class="toast-container position-fixed bottom-0 end-0 p-3">
+    <div id="liveToast" class="toast text-bg-${color}" role="alert" aria-live="assertive" aria-atomic="true">
+      <div class="toast-header">
+        <strong class="me-auto">${title}</strong>
+        <button type="button" class="btn-close" data-bs-dismiss="toast" aria-label="Close"></button>
+      </div>
+      <div class="toast-body">
+      ${body}
+      </div>
+    </div>
+  </div>
+  `);
+
+  const toastLiveExample = $("#liveToast");
+  const toastBootstrap = bootstrap.Toast.getOrCreateInstance(toastLiveExample);
+  toastBootstrap.show();
+}
+
+
+const quantidadeAutores = Number($(`input[name="quantidadeAutores"]`).val());
+const quantidadeGeneros = Number($(`input[name="quantidadeGeneros"]`).val());
+
+if (!quantidadeAutores || !quantidadeGeneros) {
+  showToast("danger", "Adicionar Livro", "Sem autores e gêneros encontrados! <br> Não será possível adicionar um livro.");
+}
